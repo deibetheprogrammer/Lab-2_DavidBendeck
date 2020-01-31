d4b32003be0b21e55e9434f7f1c85675bad8ff46
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-01-31 16:41:46 -0600

    He agregado todas las opciones excepto combate

--- src/lab/pkg2_davidbendeck/Lab2_DavidBendeck.java
+++ src/lab/pkg2_davidbendeck/Lab2_DavidBendeck.java
@@ -37,7 +37,8 @@ public class Lab2_DavidBendeck {
                     + "2) Modificar personajes\n"
                     + "3) Ver atributos del personaje\n"
                     + "4) Eliminar personaje\n"
-                    + "5) Salir\n"
+                    + "5) Combate\n"
+                    + "6) Salir\n"
                     + "Su opción: ");
             int opcion = sc.nextInt();
             vs = sc.nextLine();
@@ -82,18 +83,118 @@ public class Lab2_DavidBendeck {
                     
                     personajes.get(personajes.size()-1).imprimir();
                     break;
+                    
                 case 2:
                     System.out.println("Personajes: ");
                     
+                    int posicion = 0;
                     for (Personaje p : personajes) {
-                        
+                        System.out.println("# " +posicion + " : ");
+                        p.imprimir();
                     }
+                    
+                    System.out.print("Ingrese la posicion del personaje que desea modificar: ");
+                    posicion = sc.nextInt();
+                    vs = sc.nextLine();
+                    
+                    System.out.print("Modificar: \n"
+                            + "1) Clase\n"
+                            + "2) Nombre"
+                            + "3) Raza\n"
+                            + "4) Estatura\n"
+                            + "5) Peso\n"
+                            + "6) Edad\n"
+                            + "7) Descripcion\n"
+                            + "8) Nacionalidad\n"
+                            + "Su eleccion: ");
+                    int mod =  sc.nextInt();
+                    vs = sc.nextLine();
+                    
+                    switch (mod) {
+                        case 1:
+                            System.out.println("Elija una clase (Clerigo,Barbaro,Mago,Picaro) si ingresa una clase invalida el default sera Barbaro: ");
+                            clase = sc.nextLine();
+                            
+                            personajes.get(posicion).setClase(clase);
+                            break;
+                        case 2:
+                            System.out.print("Elija un nombre: ");
+                            nombre = sc.nextLine();
+                            
+                            personajes.get(posicion).setNombre(nombre);
+                            break;
+                        case 3:
+                            System.out.println("Elija una raza (Mediano,Enano,Elfo,Humano) si ingresa una raza invalida el default sera Humano: ");
+                            raza = sc.nextLine();
+                            
+                            personajes.get(posicion).setRaza(raza);
+                            break;
+                        case 4:
+                            System.out.print("Ingrese la estatura en cm: ");
+                            estatura = sc.nextInt();
+                            vs = sc.nextLine();
+                            
+                            personajes.get(posicion).setEstatura(estatura);
+                            break;
+                        case 5:
+                            System.out.print("Ingrese el peso en libras: ");
+                            peso = sc.nextInt();
+                            vs = sc.nextLine();
+                            
+                            personajes.get(posicion).setPeso(peso);
+                            break;
+                        case 6:
+                            System.out.print("Ingrese la edad en años: ");
+                            edad = sc.nextInt();
+                            vs = sc.nextLine();
+                            
+                            personajes.get(posicion).setEdad(edad);
+                            break;
+                        case 7:
+                            System.out.print("Ingrese una descripción: ");
+                            descripcion = sc.nextLine();
+                            
+                            personajes.get(posicion).setDescripcion(descripcion);
+                            break;
+                        case 8:
+                            System.out.println("Elija una nacionalidad (Norfair,Brinstar,Maridia,Zebes,Crateria) si ingresa una nacionalidad invalida el default sera Zebes: ");
+                            nacionalidad = sc.nextLine();
+                            
+                            personajes.get(posicion).setNacionalidad(nacionalidad);
+                            break;
+                        default:
+                            System.out.println("Opcion incorrecta");
+                    }
+                    
                     break;
                 case 3:
+                    System.out.print("Ingrese la posicion del personaje que desea revisar: ");
+                    posicion = sc.nextInt();
+                    vs = sc.nextLine();
+                    
+                    personajes.get(posicion).imprimir();
                     break;
+                    
                 case 4:
+                    
+                    System.out.println("Personajes: ");
+                    
+                    posicion = 0;
+                    for (Personaje p : personajes) {
+                        System.out.println("# " +posicion + " : ");
+                        p.imprimir();
+                    }
+                    
+                    System.out.print("Ingrese la posicion del personaje que desea eliminar: ");
+                    posicion = sc.nextInt();
+                    vs = sc.nextLine();
+                    
+                    personajes.remove(posicion);
                     break;
                 case 5:
+                    
+                    break;
+                case 6:
                     break OUTER;
             }
         }
